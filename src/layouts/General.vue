99be3aac2d02e8e9e2cb19b2be754ed9fed565ab
lucky404 <luckydewa123@gmail.com>
2022-04-07 14:15:05 +0700

    chore: prepare app layout

--- src/layouts/General.vue
+++ src/layouts/General.vue
@@ -1,8 +1,7 @@
 <template>
   <fragment>
-    <app-bar-component @toggleDrawer="toggleDrawerHandler" />
-    <sidebar-component v-model="isShowDrawer" />
-    <v-main class="accent">
+    <app-bar-component />
+    <v-main>
       <router-view />
     </v-main>
   </fragment>
@@ -12,25 +11,11 @@
 import { Fragment } from 'vue-frag';
 
 import AppBarComponent from './components/AppBar';
-import SidebarComponent from './components/Sidebar';
 
 export default {
   components: {
     Fragment,
     AppBarComponent,
-    SidebarComponent,
-  },
-
-  data() {
-    return {
-      isShowDrawer: false,
-    };
-  },
-
-  methods: {
-    toggleDrawerHandler() {
-      this.isShowDrawer = !this.isShowDrawer;
-    },
   },
 };
 </script>
--- src/layouts/components/AppBar.vue
+++ src/layouts/components/AppBar.vue
@@ -1,75 +1,25 @@
 <template>
-  <fragment>
-    <v-app-bar app color="#fff" outlined fixed flat extension-height="90">
-      <v-row no-gutters>
-        <v-col cols="12" class="d-flex align-center">
-          <v-app-bar-nav-icon v-if="isMobile" @click="toggleDrawerHandler" />
-          <v-img
-            v-if="isMobile"
-            contain
-            max-width="30"
-            max-height="30"
-            :src="require('@/assets/logo.svg')"
-            class="mr-4"
-          />
-          <v-spacer />
-          <v-btn icon>
-            <v-icon color="secondary">mdi-bell-outline</v-icon>
+  <v-app-bar app outlined flat>
+      <v-toolbar-title>Random User Table</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn @click="toggleTheme" icon v-on="on" v-bind="attrs">
+            <v-icon v-if="$vuetify.theme.dark"> mdi-weather-night </v-icon>
+            <v-icon v-else>mdi-weather-sunny</v-icon>
           </v-btn>
-          <profile-menu-component />
-        </v-col>
-      </v-row>
-      <template v-slot:extension v-if="hasAppBarExtension">
-        <v-row no-gutters>
-          <v-col cols="12">
-            <portal-target name="app-bar-extension" />
-          </v-col>
-        </v-row>
-      </template>
-    </v-app-bar>
-  </fragment>
+        </template>
+        <span>Toggle Theme</span>
+      </v-tooltip>
+  </v-app-bar>
 </template>
 
 <script>
-import { Fragment } from 'vue-frag';
-
-import VuexModule from '@/utils/vuex';
-
-import * as ROOTTYPES from '@/store-namespace/root/types';
-
-const rootModule = VuexModule(ROOTTYPES.MODULE_NAME);
-
-import ProfileMenuComponent from './ProfileMenu';
-
 export default {
-  components: {
-    Fragment,
-    ProfileMenuComponent,
-  },
-
-  computed: {
-    isMobile() {
-      return this.$vuetify.breakpoint.smAndDown;
-    },
-
-    ...rootModule.mapState({
-      hasAppBarExtension: (state) => state.hasAppBarExtension,
-    }),
-  },
-
   methods: {
-    toggleDrawerHandler() {
-      this.$emit('toggleDrawer');
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
     },
   },
 };
 </script>
-
-<style lang="scss">
-.v-app-bar {
-  border-bottom: 2px solid #eee !important;
-}
-.v-toolbar__content {
-  border-bottom: 2px solid #eee !important;
-}
-</style>
