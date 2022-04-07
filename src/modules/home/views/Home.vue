4fbdeb19e9aafdaa6cbc8ab8150d7ec25fb4067d
lucky404 <luckydewa123@gmail.com>
2022-04-07 14:22:53 +0700

    chore: remove unused modules

--- src/modules/home/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-container style="height: 1000px">
-    <v-row>
-      <v-col>
-        <h1>Welcome to Sentinel Panel</h1>
-      </v-col>
-    </v-row>
-  </v-container>
-</template>
