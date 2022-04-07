c60d3a60ea28dbfdf8d9d4af0f9e03fe30b74e14
lucky404 <luckydewa123@gmail.com>
2022-04-07 16:51:10 +0700

    feat: add dark and light theme

--- src/layouts/components/AppBar.vue
+++ src/layouts/components/AppBar.vue
@@ -1,16 +1,16 @@
 <template>
   <v-app-bar app outlined flat>
-      <v-toolbar-title>Random User Table</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn @click="toggleTheme" icon v-on="on" v-bind="attrs">
-            <v-icon v-if="$vuetify.theme.dark"> mdi-weather-night </v-icon>
-            <v-icon v-else>mdi-weather-sunny</v-icon>
-          </v-btn>
-        </template>
-        <span>Toggle Theme</span>
-      </v-tooltip>
+    <v-toolbar-title>Random User Table</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn @click="toggleTheme" icon v-on="on" v-bind="attrs">
+          <v-icon v-if="$vuetify.theme.dark"> mdi-weather-night </v-icon>
+          <v-icon v-else>mdi-weather-sunny</v-icon>
+        </v-btn>
+      </template>
+      <span>Toggle Theme</span>
+    </v-tooltip>
   </v-app-bar>
 </template>
 
