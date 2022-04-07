0126ab16c013ca70553204ecf79453a6109c0a21
lucky404 <luckydewa123@gmail.com>
2022-04-07 14:23:17 +0700

    chore: remove unused store modules

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,9 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import root from './modules/root';
-import authentication from './modules/authentication';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { root, authentication },
+  modules: { root },
 });
